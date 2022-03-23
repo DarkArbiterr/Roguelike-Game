be548462d5ab5aca860d43aa1537d51e9db88045
DarkArbiter <zajac.p@onet.pl>
2022-03-23 17:55:32 +0100

    Wall collisions
    Add wall collisions

--- Roguelike Game Project/Assets/Scripts/PlayerControler.cs
+++ Roguelike Game Project/Assets/Scripts/PlayerControler.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
+    public static PlayerControler instance;
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
-    Vector2 movement;
     private float shootHorizontal, shootVertical;
     public GameObject bulletPrefab;
     public float bulletSpeed;
     private float lastFire;
     public float fireDelay;
+    public Vector2 move;
+    private Vector3 moveDirection;
+    
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-
         shootHorizontal = Input.GetAxis("ShootHorizontal");
         shootVertical = Input.GetAxis("ShootVertical");
 
@@ -27,11 +27,116 @@ public class PlayerControler : MonoBehaviour
             lastFire = Time.time;
         }
 
+        //move.x = Input.GetAxisRaw("Horizontal");
+        //move.y = Input.GetAxisRaw("Vertical");
+
+        move = new Vector2(0f, 0f);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            move.y = +1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            move.y = -1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            move.x = -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            move.x = +1f;
+        }
+
+        moveDirection = new Vector3(move.x, move.y).normalized;
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.velocity = moveDirection * moveSpeed;
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 movement = new Vector2(0f, 0f);
+        bool isIdle;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            movement.y = +1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            movement.y = -1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            movement.x = -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            movement.x = +1f;
+        }
+
+        isIdle = movement.x == 0 && movement.y == 0;
+
+        if (isIdle)
+        {
+            // TODO - animacja "player idle"
+        }
+        else
+        {
+            Vector3 moveDirection = new Vector3(movement.x, movement.y).normalized;
+            Vector3 targetMovePosition = transform.position + moveDirection * moveSpeed * Time.deltaTime;
+            RaycastHit2D raycastHit = Physics2D.Raycast(transform.position, moveDirection, moveSpeed * Time.deltaTime, LayerMask.GetMask("Blocking"));
+            if (raycastHit.collider == null)
+            {
+                // Można się ruszyć, nie napotkano przeszkody
+                // TODO - lastMoveDirection do animacji "player idle"
+                // TODO - animacja chodzenia
+                transform.position = targetMovePosition;
+            }
+            else
+            {
+                // Nie można się ruszyć Diagonalnie, napotkano przeszkodę
+                Vector3 testMoveDirection = new Vector3(moveDirection.x, 0f).normalized;
+                targetMovePosition = transform.position + testMoveDirection * moveSpeed * Time.deltaTime;
+                raycastHit = Physics2D.Raycast(transform.position, testMoveDirection, moveSpeed * Time.deltaTime, LayerMask.GetMask("Blocking"));
+                if (testMoveDirection.x != 0f && raycastHit.collider == null)
+                {
+                    // Można się ruszyć Horyzontalnie
+                    // TODO - lastMoveDirection do animacji "player idle"
+                    // TODO - animacja chodzenia
+                    transform.position = targetMovePosition;
+                }
+                else
+                {
+                    // Nie można ruszyć się Horyzontalnie
+                    testMoveDirection = new Vector3(0f, moveDirection.y).normalized;
+                    targetMovePosition = transform.position + testMoveDirection * moveSpeed * Time.deltaTime;
+                    raycastHit = Physics2D.Raycast(transform.position, testMoveDirection, moveSpeed * Time.deltaTime, LayerMask.GetMask("Blocking"));
+                    if (testMoveDirection.y != 0f && raycastHit.collider == null)
+                    {
+                        // Można ruszyć się Wertykalnie
+                        // TODO - lastMoveDirection do animacji "player idle"
+                        // TODO - animacja chodzenia
+                        transform.position = targetMovePosition;
+                    }
+                    else
+                    {
+                        // Nie można ruszyć się Wertykalnie
+                        // Animacja player idle
+                    }
+                }
+            }
+        }
+
     }
 
     void Shoot(float x, float y)
